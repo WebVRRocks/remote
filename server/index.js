3d6f303a2af33e967ae1b22bf37fa0edbc18e3f6
Christopher Van Wiemeersch <hearcomestreble@gmail.com>
2017-10-04 18:56:19 -0700

    hook up server with Express + RedisDown for PouchDB

--- server/index.js
+++ server/index.js
@@ -1,14 +1,22 @@
-require('dotenv').config();
-
 const fs = require('fs');
 const http = require('http');
 const path = require('path');
 const url = require('url');
 
 const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
 const ecstatic = require('ecstatic');
+const express = require('express');
+const expressPouchDB = require('express-pouchdb');
+const PouchDB = require('pouchdb');
+const resisdown = require('redisdown');
 const SocketPeer = require('socketpeer');
+const trailingSlash = require('trailing-slash');
 const twilio = require('twilio');
+
+dotenv.config({
+  path: path.join(__dirname, '..')
+});
 
 const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
@@ -21,7 +29,12 @@ const corsHeaders = {
 const host = process.env.SOCKETPEER_HOST || process.env.HOST || '0.0.0.0';
 const port = process.env.SOCKETPEER_PORT || process.env.PORT || 3000;
 const nodeEnv = process.env.NODE_ENV || 'development';
-const httpServer = http.createServer();
+
+const app = express();
+
+app.use(trailingSlash({slash: true});
+
+const httpServer = http.createServer(app);
 const ecstaticMiddleware = ecstatic({
   root: __dirname,
   headers: corsHeaders
@@ -136,6 +149,78 @@ function cors (req, res) {
   return true;
 }
 
+function sms (req, res) {
+  const contentType = req.headers['content-type'] || '';
+  const parser = contentType.includes('json') ? bodyParser.json() : bodyParser.urlencoded({extended: false});
+
+  // Values taken from the Twilio dashboard:
+  //
+  //   https://www.twilio.com/console
+  //
+  // Locally, these values are stored in the `.env` in the root directory,
+  // which is not checked in to the Git repository (so ask @cvan for the details).
+  //
+  // In production (Heroku), values are stored as environment values:
+  //
+  //   https://dashboard.heroku.com/apps/webxr-remote/settings#ember1901
+  //
+  const twilioAccountSid = process.env.TWILIO_ACCOUNT_SID;
+  const twilioAuthToken = process.env.TWILIO_AUTH_TOKEN;
+  const twilioPhoneNumber = process.env.TWILIO_PHONE_NUMBER;
+
+  if (!twilioAccountSid || !twilioAuthToken || !twilioPhoneNumber) {
+    let twilioErr;
+    if (!twilioAccountSid) {
+      twilioErr = new Error('Expected environment variable `TWILIO_ACCOUNT_SID` to be set (ask @cvan)');
+    }
+    if (!twilioAuthToken) {
+      twilioErr = new Error('Expected environment variable `TWILIO_AUTH_TOKEN` to be set (ask @cvan)');
+    }
+    if (!twilioPhoneNumber) {
+      twilioErr = new Error('Expected environment variable `TWILIO_PHONE_NUMBER` to be set (ask @cvan)');
+    }
+    console.warn(twilioErr);
+    jsonBody(req, res, {error: {message: twilioErr.message || 'Unknown error'}}, 400);
+    return;
+  }
+
+  const twilioClient = new twilio(twilioAccountSid, twilioAuthToken);
+
+  return parser(req, res, next);
+
+  function next () {
+    const smsBody = req.body.body;
+    let smsTo = req.body.to;
+    return new Promise((resolve, reject) => {
+      if (!smsBody) {
+        throw new Error('Value missing for `body` field (e.g., `Check this out!`)');
+      }
+      if (!smsTo) {
+        throw new Error('Value missing for `to` field (e.g., `+16505551212`)');
+      }
+      smsTo = parsePhoneNumber(smsTo);
+      if (!smsTo) {
+        throw new Error('Unexpected value for `to` field (e.g., `+16505551212`)');
+      }
+      return twilioClient.messages.create({
+        body: smsBody,
+        to: smsTo,
+        from: twilioPhoneNumber
+      }, function (err, msg) {
+        if (err) {
+          return reject(err);
+        }
+        resolve(msg);
+      });
+    }).then(msg => {
+      jsonBody(req, res, {success: true, sid: msg.sid}, 200);
+    }).catch(err => {
+      console.warn(err);
+      jsonBody(req, res, {error: {message: err.message || 'Unknown error'}}, 400);
+    });
+  }
+}
+
 httpServer.on('request', (req, res) => {
   const urlParsed = url.parse(req.url);
   const pathname = urlParsed.pathname;
@@ -153,91 +238,36 @@ httpServer.on('request', (req, res) => {
   if (pathname === '/') {
     return redirect(req, res, '/' + generatePinCode());
   }
-  if (pathname === '/sms/') {
-    return redirect(req, res, '/sms');
-  }
   if (pathname === '/sms') {
-    const contentType = req.headers['content-type'] || '';
-    const parser = contentType.includes('json') ? bodyParser.json() : bodyParser.urlencoded({extended: false});
-
-    // Values taken from the Twilio dashboard: https://www.twilio.com/console
-    // Stored in `.env` locally (i.e., this file is not checked in to Git repository, so ask @cvan).
-    // The environment values are stored in production on Heroku: https://dashboard.heroku.com/apps/webxr-remote/settings#ember1901
-    const twilioAccountSid = process.env.TWILIO_ACCOUNT_SID;
-    const twilioAuthToken = process.env.TWILIO_AUTH_TOKEN;
-    const twilioPhoneNumber = process.env.TWILIO_PHONE_NUMBER;
-
-    if (!twilioAccountSid || !twilioAuthToken || !twilioPhoneNumber) {
-      let twilioErr;
-      if (!twilioAccountSid) {
-        twilioErr = new Error('Expected environment variable `TWILIO_ACCOUNT_SID` to be set (ask @cvan)');
-      }
-      if (!twilioAuthToken) {
-        twilioErr = new Error('Expected environment variable `TWILIO_AUTH_TOKEN` to be set (ask @cvan)');
-      }
-      if (!twilioPhoneNumber) {
-        twilioErr = new Error('Expected environment variable `TWILIO_PHONE_NUMBER` to be set (ask @cvan)');
-      }
-      console.warn(twilioErr);
-      jsonBody(req, res, {error: {message: twilioErr.message || 'Unknown error'}}, 400);
-      return;
-    }
-
-    const twilioClient = new twilio(twilioAccountSid, twilioAuthToken);
-
-    parser(req, res, next);
-
-    function next () {
-      const smsBody = req.body.body;
-      let smsTo = req.body.to;
-      return new Promise((resolve, reject) => {
-        if (!smsBody) {
-          throw new Error('Value missing for `body` field (e.g., `Check this out!`)');
-        }
-        if (!smsTo) {
-          throw new Error('Value missing for `to` field (e.g., `+16505551212`)');
-        }
-        smsTo = parsePhoneNumber(smsTo);
-        if (!smsTo) {
-          throw new Error('Unexpected value for `to` field (e.g., `+16505551212`)');
-        }
-        return twilioClient.messages.create({
-          body: smsBody,
-          to: smsTo,
-          from: twilioPhoneNumber
-        }, function (err, msg) {
-          if (err) {
-            return reject(err);
-          }
-          resolve(msg);
-        });
-      }).then(msg => {
-        jsonBody(req, res, {success: true, sid: msg.sid}, 200);
-      }).catch(err => {
-        console.warn(err);
-        jsonBody(req, res, {error: {message: err.message || 'Unknown error'}}, 400);
-      });
-    }
-
-    return;
+    return sms(req, res);
   }
 
   const pathnameHasPin = /^\/[0-9]+$/.test(pathname);
   if (pathnameHasPin) {
+    console.log('pathnameHasPin', pathnameHasPin);
     req.url = '/';
   }
   if (pathnameHasPin || staticPaths.includes(pathname)) {
+    console.log('ECSTATIC', pathnameHasPin, staticPaths.includes(pathname));
     return ecstaticMiddleware(req, res);
   }
+
+  console.log('not found');
 
   notFound(req, res);
 });
 
 if (!module.parent) {
+  const redisdownPouchDB = PouchDB.defaults({db: resisdown});
+
+  app.use('/db', expressPouchDB(redisdownPouchDB));
+
   httpServer.listen(port, host, () => {
     console.log('[%s] Server listening on %s:%s', nodeEnv, host, port);
   });
 }
+
+module.exports.app = app;
 
 module.exports.httpServer = httpServer;
 
